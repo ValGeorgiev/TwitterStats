56c25686615529a8be780db9e3603bdf4cf3cddb
ValentinAlexandrovGeorgiev <valentin.georgiev95@abv.bg>
2016-06-26 11:09:11 +0300

    Server configurations

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "database": "mongodb://twitter:123456789@ds021984.mlab.com:21984/twitstats",
+    "port": process.env.Port || 2016,
+    "secretKey": "MySecretKey",
+
+    "twit": {
+        consumer_key:         'PhIzLxsbCvE76RyZLZmP1Jd2t',
+        consumer_secret:      'YoQrFFcJFicj8NI3uuVwDQW0zuFa52q97fWbeLvZ5J3451RNVs',
+        access_token:         '746749847995293696-pbjViIQ84SuYHQSLp2plUhpyB7EIQFW',
+        access_token_secret:  'DIEJECxSIyXHujialVCmA59htBdPs5yyi792tJEIrAlbh'
+    }
+};
